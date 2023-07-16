b7120e1f212b62b7461fa6c7656054a72d30a89c
Andrew Fan <andrewfan030114@gmail.com>
2023-07-16 15:15:18 -0700

    Added constants and collision checker for Player

--- src/main/model/Player.java
+++ src/main/model/Player.java
@@ -1,9 +1,14 @@
 package model;
 
+import java.awt.*;
+
+
 /*
  * Represents the player.
  */
 public class Player {
+    public static final int SIZE = 15;
+    public static final int SPEED = 20;
     private int xcoord;
     private int ycoord;
 
@@ -23,29 +28,30 @@ public class Player {
     }
 
     public void moveLeft() {
-        xcoord -= 1;
+        xcoord -= SPEED;
 
         handleBoundary();
     }
 
     public void moveRight() {
-        xcoord += 1;
+        xcoord += SPEED;
 
         handleBoundary();
     }
 
     public void moveUp() {
-        ycoord += 1;
+        ycoord -= SPEED;
 
         handleBoundary();
     }
 
     public void moveDown() {
-        ycoord -= 1;
+        ycoord += SPEED;
 
         handleBoundary();
     }
 
+    //Based from Space Invader
     private void handleBoundary() {
         if (xcoord < 0) {
             xcoord = 0;
@@ -56,6 +62,17 @@ public class Player {
         } else if (ycoord > 800) {
             ycoord = 800;
         }
+    }
+
+    //Based from Space Invader
+    // Determines if player has collided with a projectile
+    // modifies: none
+    // effects:  returns true if player has collided with projectile m, false otherwise
+    public boolean checkHit(Projectile p) {
+        Rectangle playerBoundingSquare = new Rectangle(getX() - SIZE / 2, getY() - SIZE / 2, SIZE, SIZE);
+        Rectangle projectileBoundingRect = new Rectangle((int) (p.getX() - Projectile.SIZE / 2), (int) (p.getY()
+                - Projectile.SIZE / 2), Projectile.SIZE, Projectile.SIZE);
+        return playerBoundingSquare.intersects(projectileBoundingRect);
     }
 }
 
